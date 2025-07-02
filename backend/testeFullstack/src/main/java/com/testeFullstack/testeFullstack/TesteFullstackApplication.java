0da5b1ddb508d6e13ceec78fa55d9e5ad4a6397f
Andre <andrefonteneledev@gmail.com>
2025-07-01 23:06:45 -0300

    importação do backend

---
+++ backend/testeFullstack/src/main/java/com/testeFullstack/testeFullstack/TesteFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.testeFullstack.testeFullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteFullstackApplication.class, args);
+	}
+
+}
---
+++ backend/testeFullstack/src/test/java/com/testeFullstack/testeFullstack/TesteFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testeFullstack.testeFullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
